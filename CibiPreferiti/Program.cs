55de290b2f1e9ac04c5fc4e941575ec6d8db0083
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-16 15:27:52 +0200

    ADD Bonus

--- CibiPreferiti/Program.cs
+++ CibiPreferiti/Program.cs
@@ -28,6 +28,41 @@
 
             // Stampiamo il cibo meno preferito
             Console.WriteLine($"Il mio cibo meno preferito è: {ciboMenoPreferito}");
+
+            /*
+             *  BONUS
+             */
+
+            // Creamo la variabile con la lunghezza dell'array "cibiPreferiti"
+            int lengthOfCibiPreferiti = cibiPreferiti.Length;
+
+            // Se lunghezza di cibiPreferiti e un numero pari
+            if (lengthOfCibiPreferiti % 2 == 0 ) 
+            {
+                // Recuperiamo indici dei valori centrali sia quello di sinistra che di destra
+                int middleLeftIndex = lengthOfCibiPreferiti / 2 - 1;
+                int middleRightIndex = lengthOfCibiPreferiti / 2;
+
+                // Creamo le variabile con i valori centrali dell'array cibiPreferiti
+                string middleLeftOfCibiPreferiti = cibiPreferiti[middleLeftIndex]; 
+                string middleRightOfCibiPreferiti = cibiPreferiti[middleRightIndex];
+
+                // Stampiamo i valori centrali dell'array cibiPreferiti
+                Console.WriteLine($"Middle left Value: {middleLeftOfCibiPreferiti}");
+                Console.WriteLine($"Middle Right Value: {middleRightOfCibiPreferiti}");
+            } 
+            // Nel caso in cui la lunghezza di cibiPreferiti e un numero dispari
+            else
+            {
+                // Recuperiamo indice del valore centrale
+                int middleIndex = lengthOfCibiPreferiti / 2;
+
+                // Creamo la variabile con il valore centrale dell'array cibiPreferiti
+                string middleValueOfCibiPreferiti = cibiPreferiti[middleIndex];
+
+                // Stampiamo il valore centrale dell'array cibiPreferiti
+                Console.WriteLine($"Middle Value: {middleValueOfCibiPreferiti}");
+            }
         }
     }
 }
